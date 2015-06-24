758452262871238a320925560ccfad41564afe41
Piotrosz <piotrosz@gmail.com>
2015-06-24 18:20:33 +0200

    OrderByDescending

---
+++ CSharpGotchas/OrderByDescending/MaximalComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace CSharpGotchas.OrderByDescending
+{
+    // http://codeblog.jonskeet.uk/2015/03/02/backward-compatibility-pain/
+    class MaximalComparer<T> : IComparer<T>
+    {
+        private readonly IComparer<T> original;
+
+        public MaximalComparer(IComparer<T> original)
+        {
+            this.original = original;
+        }
+
+        public int Compare(T first, T second)
+        {
+            int originalResult = original.Compare(first, second);
+            return originalResult == 0 ? 0
+                : originalResult < 0 ? int.MinValue
+                : int.MaxValue;
+        }
+    }
+}
---
+++ CSharpGotchas/OrderByDescending/OrderByDescendingTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpGotchas.OrderByDescending
+{
+    // http://codeblog.jonskeet.uk/2015/03/02/backward-compatibility-pain/
+    public class OrderByDescendingTests
+    {
+        [Fact]
+        public void order_by_descending_does_not_work()
+        {
+            var comparer = new MaximalComparer<string>(Comparer<string>.Default);
+            string[] input = { "apples", "carrots", "dougnuts", "bananas" };
+
+            var sorted = input.OrderByDescending(x => x, comparer);
+
+            sorted.Should().ContainInOrder("carrots", "dougnuts", "apples", "bananas");
+        }
+    }
+}
