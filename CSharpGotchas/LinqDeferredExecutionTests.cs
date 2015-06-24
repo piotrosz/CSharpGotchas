14caf321d81cd79c386142fcd52084796556125c
Piotr Ludwiczuk <piotrosz@gmail.com>
2015-06-24 15:38:15 +0200

    Create LinqDeferredExecutionTests.cs

---
+++ CSharpGotchas/LinqDeferredExecutionTests.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpGothas
+{
+    public class LinqDeferredExecutionTests
+    {
+        [Fact]
+        public void linq_defers_execution()
+        {
+            var array = new[] {"a", "b", "a", "a"};
+
+            var filteringValue = "a";
+            var results1 = array.Where(x => x == filteringValue);
+
+            filteringValue = "b";
+            var results2 = array.Where(x => x == filteringValue);
+
+            results1.Count().Should().Be(1);
+            results2.Count().Should().Be(1);
+        }
+    }
+}
