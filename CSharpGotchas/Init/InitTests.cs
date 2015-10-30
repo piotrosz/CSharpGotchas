739b5dab6b8118e9a5f61ee96f43e9ca1c94f254
unknown <piotrosz@gmail.com>
2015-10-30 13:28:34 +0100

    Added class initialize gotcha

---
+++ CSharpGotchas/Init/InitTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace CSharpGotchas.Init
+{
+    public class InitTests
+    {
+        [Fact]
+        public void when_exception_is_thrown_during_shorthand_init_then_object_is_null()
+        {
+            SampleClass target = null;
+
+            try
+            {
+                target = new SampleClass { X = 1, Y = 2 };
+            }
+            catch
+            {
+            }
+
+            target.Should().BeNull();
+        }
+
+        [Fact]
+        public void when_exception_is_thrown_during_init_then_object_is_not_null()
+        {
+            SampleClass target = null;
+
+            try
+            {
+                target = new SampleClass();
+                target.X = 1;
+                target.Y = 2;
+            }
+            catch
+            {
+            }
+
+            target.Should().NotBeNull();
+        }
+    }
+}
---
+++ CSharpGotchas/Init/SampleClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharpGotchas.Init
+{
+    public class SampleClass
+    {
+        public int X { get; set; }
+
+        public int Y
+        {
+            get { return -1; } 
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
